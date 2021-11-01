9d6f357daa523e3b7d77583b2cb63e38b23cf787
Sumit-Singh-8 <beingdevil8950@gmail.com>
2021-11-01 11:16:53 +0530

    fix menu bar btn

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-
 const add = () => {
-    menu_bar.classList.toggle("menu-active");
-    if(close.style.display == "none"){
-        close.style.display = "block";
-        open.style.display = "none";
-    }
-    else{
-        close.style.display = "none";
-        open.style.display = "block";
+  menu_bar.classList.toggle("menu-active");
+};
 
-    }
-}
-let close = document.querySelector("#close")
-let open = document.querySelector("#open")
+const opens = () => {
+  open.style.display = "none";
+  close.style.display = "block";
+};
+
+const closes = () => {
+  close.style.display = "none";
+  open.style.display = "block";
+};
+
+let close = document.querySelector("#close");
+let open = document.querySelector("#open");
 let menu_bar = document.querySelector(".menu");
 let menu_open = document.querySelector(".menu-btn");
 menu_open.addEventListener("click", add);
+open.addEventListener("click", opens);
+close.addEventListener("click", closes);
